a348a370adca2cd2a7cfe85e77188cd8710781a7
Larry Lo <larrylo@capax-tech.com>
2023-12-14 17:26:18 +0800

    fix import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_deck/flutter_deck.dart';
-import 'package:flutter_deck_example/slides/slides.dart';
+import 'package:git_introduction/slides/end_slide.dart';
+import 'package:git_introduction/slides/quote_slide.dart';
+import 'package:git_introduction/slides/title_slide.dart';
+
+
 
 void main() {
   runApp(const FlutterDeckExample());
